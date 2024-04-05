862fcccf468faafc292851a5b0dec8186a332106
Marjorie Luize <90302120+AriaGrr@users.noreply.github.com>
2024-04-05 16:10:39 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+# Turma 725, Equipe 4:
+# Marjorie Luize Martins Costa, RA: 24223084-5
+# Paulo Andre de Oliveira Hirata, RA: 24.123.086-1
+# Diogo Santos Linna, RA: 24.123.003-6
+# Victor Merker Binda, RA:
+
+# Bibliotecas
+import math
+from decimal import Decimal
+
+# Variaveis globais
+teste = Decimal(0) # Variável para testar se é a primeira vez que o programa é executado
+
+# Função para limpar a tela do terminal
+def clear_screen():
+    print('\033[H\033[J')
+
+# Menu principal
+while True:
+    clear_screen()
+
+    if teste == Decimal(0):
+        print('OEM: Ondas Eletromagnéticas com Python')
+        print('Desnvolvedores: Marjorie Luize Martins Costa, Paulo Andre de Oliveira Hirata, Diogo Santos Linna, Victor Merker Binda')
+        # Melhorar a descrição do programa
+        print('Descrição')
+        #
+        print('Pressione Enter para continuar...')
+        input() 
+        teste = Decimal(1)
+
+    print('Opções:')
+    print('1 - Teste')
+    print('2 - Teste')
+    print('3 - Teste')
+    print('0 - Sair')
+    option = input('Escolha uma opção: ')
+
+    if option == '1':
+        print('Teste')
+
+    elif option == '2':
+        print('Teste')
+
+    elif option == '3':
+        print('Limpando variáveis...')
+
+    elif option == '0':
+        break
+    else:
+        print('Opção inválida. Escolha uma opção válida.')
+
+    input('Pressione Enter para continuar...')
+
+print('Finalizando...')
