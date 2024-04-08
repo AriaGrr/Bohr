faacb116a40397b801ca33853b38ee922c6c7ce5
Marjorie Luize <90302120+AriaGrr@users.noreply.github.com>
2024-04-08 22:29:22 +0000

    corrigindo

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from decimal import Decimal
 
 # Variaveis globais
 teste = Decimal(0) # Variável para testar se é a primeira vez que o programa é executado
+ow = 0 # Variável para armazenar a opção escolhida no menu de conversores  
 
 # Constantes
 h = 6.62607015 * 10 ** -34 # Constante de Planck em J.s
@@ -103,39 +104,139 @@ def metros_nm(num):
 
 # Opções do menu de conversores imbutido
 
-def conversores(x):
-    global num, num_c
-    num = x # Recebe o valor a ser convertido
-    # Conversor de metros, colocar as saidas que saem em metros
-    if x == r or x == v or x == lE:
-        print('Digite a saida desejada: ')
-        print('1 - cm')
-        print('2 - nm')
-        print('3 - km')
-        print('4 - mm')
-        print('5 - um')
-        print('6 - pm')
-        option = input()
-        if option == '1':
-            print ('Opção 1 selecionada...')
-            # Colocar logicas de conversão
-        elif option == '2':
-            print ('Opção 2 selecionada...')
-            metros_nm(num)
-            print(f'{num:.4g} m = {num_c:.4g} nm')
+def conversores(y):
+    global num, num_c, teste2
 
-        elif option == '3':
-            print ('Opção 3 selecionada...')
-            # Colocar logicas de conversão
-        elif option == '4':
-            print ('Opção 4 selecionada...')
-            # Colocar logicas de conversão
-        elif option == '5':
-            print ('Opção 5 selecionada...')
-            # Colocar logicas de conversão
-        elif option == '6':
-            print ('Opção 6 selecionada...')
-            # Colocar logicas de conversão
+    if teste2 == 0:
+        print('Deseja fazer uma conversão? (S/N)')
+
+        op = input()
+
+    if op == 'S' or 's':
+        ow = 1
+    while ow == 1:
+        print('Escolha o valor que deseja converter: ')
+        x = input()
+        if y == 1:
+            if x == 1:
+                num == r
+            elif x == 2:
+                num == v
+            elif x == 3:
+                num == K
+            elif x == 4:
+                num == U
+            elif x == 5:
+                num == E
+            elif x == 6:
+                num == lE
+        elif y == 2:
+            if x == 1:
+                num == 1
+            elif x == 2:
+                num == 1
+            elif x == 3:
+                num == 1
+            elif x == 4:
+                num == 1
+            elif x == 5:
+                num == 1
+            elif x == 6:
+                num == 1
+            elif x == 7:
+                num == 1
+        elif y == 3:
+            if x == 1:
+                num == 1
+            elif x == 2:
+                num == 1
+            elif x == 3:
+                num == 1
+            elif x == 4:
+                num == 1
+            elif x == 5:
+                num == 1
+            elif x == 6:
+                num == 1
+        elif y == 4:
+            if x == 1:
+                num == 1
+            elif x == 2:
+                num == 1
+            elif x == 3:
+                num == 1
+            elif x == 4:
+                num == 1
+            elif x == 5:
+                num == 1
+            elif x == 6:
+                num == 1
+        elif y == 5:
+            if x == 1:
+                num == 1
+            elif x == 2:
+                num == 1
+            elif x == 3:
+                num == 1
+            elif x == 4:
+                num == 1
+            elif x == 5:
+                num == 1
+            elif x == 6:
+                num == 1
+        elif y == 6:
+            if x == 1:
+                num == 1
+        else:
+            print('Opção inválida. Escolha uma opção válida.')
+        print('Digite a saida desejada: ')
+        if x == r or x == v or x == lE:
+            print('1 - cm')
+            print('2 - nm')
+            print('3 - km')
+            print('4 - mm')
+            print('5 - um')
+            print('6 - pm')
+            output = input()
+            if output == '1':
+                print ('Opção 1 selecionada...')
+                # Colocar logicas de conversão
+            elif output == '2':
+                print ('Opção 2 selecionada...')
+                metros_nm(num)
+                print(f'{num:.4g} m = {num_c:.4g} nm')
+            elif output == '3':
+                print ('Opção 3 selecionada...')
+                # Colocar logicas de conversão
+            elif output == '4':
+                print ('Opção 4 selecionada...')
+                # Colocar logicas de conversão
+            elif output == '5':
+                print ('Opção 5 selecionada...')
+                # Colocar logicas de conversão
+            elif output == '6':
+                print ('Opção 6 selecionada...')
+                # Colocar logicas de conversão
+            else:
+                print('Opção inválida. Escolha uma opção válida.')
+            
+
+        elif x == K or x == U or x == E:
+            print('1 - eV')
+            print('2 - J')
+            print('3 - cal')
+            print('4 - kcal')
+            print('5 - BTU')
+            print('6 - kWh')
+            print('7 - Wh')
+
+
+
+    else:
+        print('Voltando ao menu...')
+        input('Pressione Enter para continuar...')
+        limpar_variaveis()
+        menu()
         
 def escolhas1():
     print('Digite o número da opção que deseja converter: ')
@@ -191,15 +292,7 @@ def opcao1():
     print(f'5 - Energia total (E): {E:.4g} eV')
     print(f'6 - Comprimento de onda do elétron (lE): {lE:.4g} m')
 
-    print('Deseja converter algum valor da saida? (S/N)')
-    option = input()
-    if option == 'S' or option == 's':
-
-    else:
-        print('Voltando ao menu...')
-        input('Pressione Enter para continuar...')
-        limpar_variaveis()
-        menu()
+    conversores(1)
     
     input('Pressione Enter para continuar...')
     limpar_variaveis()
