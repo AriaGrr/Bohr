7028a6eb6502f262c1f5968d7f4f59318833bf3b
Marjorie Luize <90302120+AriaGrr@users.noreply.github.com>
2024-04-08 23:20:13 +0000

    teste

--- main.py
+++ main.py
@@ -104,188 +104,146 @@ def metros_nm(num):
 
 # Opções do menu de conversores imbutido
 
-def conversores(y):
-    global num, num_c, ow
+# def conversores2(y):
+#     global num_c, r, v, K, U, E, lE, lF, f, eF, ow, num
 
-    print('Deseja fazer uma conversão? (S/N)')
-    op = input()
+#     valor = y
+#     print('Digite a saida desejada: ')
+#     print (y)
+#     if y == r or y == v or y == lE:
+#         print('1 - cm')
+#         print('2 - nm')
+#         print('3 - km')
+#         print('4 - mm')
+#         print('5 - um')
+#         print('6 - pm')
+#         output = input()
+#         if output == '1':
+#             print ('Opção 1 selecionada...')
+#                 # Colocar logicas de conversão
+#         elif output == '2':
+#             print ('Opção 2 selecionada...')
+#             metros_nm(num)
+#             print(f'{num:.4g} m = {num_c:.4g} nm')
+#         elif output == '3':
+#             print ('Opção 3 selecionada...')
+#                 # Colocar logicas de conversão
+#         elif output == '4':
+#             print ('Opção 4 selecionada...')
+#                 # Colocar logicas de conversão
+#         elif output == '5':
+#             print ('Opção 5 selecionada...')
+#                 # Colocar logicas de conversão
+#         elif output == '6':
+#             print ('Opção 6 selecionada...')
+#                 # Colocar logicas de conversão
+#         else:
+#             print('Opção inválida. Escolha uma opção válida.')
 
-    if op == 'S' or 's':
-        ow = 1
-    while ow == 1:
-        ow = 0
-        print('Escolha o valor que deseja converter: ')
-        x = input()
-        if y == 1:
-            if x == 1:
-                num == r
-            elif x == 2:
-                num == v
-            elif x == 3:
-                num == K
-            elif x == 4:
-                num == U
-            elif x == 5:
-                num == E
-            elif x == 6:
-                num == lE
-        elif y == 2:
-            if x == 1:
-                num == 1
-            elif x == 2:
-                num == 1
-            elif x == 3:
-                num == 1
-            elif x == 4:
-                num == 1
-            elif x == 5:
-                num == 1
-            elif x == 6:
-                num == 1
-            elif x == 7:
-                num == 1
-        elif y == 3:
-            if x == 1:
-                num == 1
-            elif x == 2:
-                num == 1
-            elif x == 3:
-                num == 1
-            elif x == 4:
-                num == 1
-            elif x == 5:
-                num == 1
-            elif x == 6:
-                num == 1
-        elif y == 4:
-            if x == 1:
-                num == 1
-            elif x == 2:
-                num == 1
-            elif x == 3:
-                num == 1
-            elif x == 4:
-                num == 1
-            elif x == 5:
-                num == 1
-            elif x == 6:
-                num == 1
-        elif y == 5:
-            if x == 1:
-                num == 1
-            elif x == 2:
-                num == 1
-            elif x == 3:
-                num == 1
-            elif x == 4:
-                num == 1
-            elif x == 5:
-                num == 1
-            elif x == 6:
-                num == 1
-        elif y == 6:
-            if x == 1:
-                num == 1
-        else:
-            print('Opção inválida. Escolha uma opção válida.')
-        print('Digite a saida desejada: ')
-        if x == r or x == v or x == lE:
-            print('1 - cm')
-            print('2 - nm')
-            print('3 - km')
-            print('4 - mm')
-            print('5 - um')
-            print('6 - pm')
-            output = input()
-            if output == '1':
-                print ('Opção 1 selecionada...')
-                # Colocar logicas de conversão
-            elif output == '2':
-                print ('Opção 2 selecionada...')
-                metros_nm(num)
-                print(f'{num:.4g} m = {num_c:.4g} nm')
-            elif output == '3':
-                print ('Opção 3 selecionada...')
-                # Colocar logicas de conversão
-            elif output == '4':
-                print ('Opção 4 selecionada...')
-                # Colocar logicas de conversão
-            elif output == '5':
-                print ('Opção 5 selecionada...')
-                # Colocar logicas de conversão
-            elif output == '6':
-                print ('Opção 6 selecionada...')
-                # Colocar logicas de conversão
-            else:
-                print('Opção inválida. Escolha uma opção válida.')
-            
+#     elif num == K or num == U or num == E:
+#         print('1 - eV')
+#         print('2 - J')
+#         print('3 - cal')
+#         print('4 - kcal')
+#         print('5 - BTU')
+#         print('6 - kWh')
+#         print('7 - Wh')
 
-        elif x == K or x == U or x == E:
-            print('1 - eV')
-            print('2 - J')
-            print('3 - cal')
-            print('4 - kcal')
-            print('5 - BTU')
-            print('6 - kWh')
-            print('7 - Wh')
+#         output = input()
+#         if output == '1':
+#             print ('Opção 1 selecionada...')
+#                 # Colocar logicas de conversão
+#         elif output == '2':
+#             print ('Opção 2 selecionada...')
+#                 # Colocar logicas de conversão
+#         elif output == '3':
+#             print ('Opção 3 selecionada...')
+#                 # Colocar logicas de conversão
+#         elif output == '4':
+#             print ('Opção 4 selecionada...')
+#                 # Colocar logicas de conversão
+#         elif output == '5':
+#             print ('Opção 5 selecionada...')
+#                 # Colocar logicas de conversão
+#         elif output == '6':
+#             print ('Opção 6 selecionada...')
+#                 # Colocar logicas de conversão
+#         elif output == '7':
+#             print ('Opção 7 selecionada...')
+#                 # Colocar logicas de conversão
 
-            output = input()
-            if output == '1':
-                print ('Opção 1 selecionada...')
-                # Colocar logicas de conversão
-            elif output == '2':
-                print ('Opção 2 selecionada...')
-                # Colocar logicas de conversão
-            elif output == '3':
-                print ('Opção 3 selecionada...')
-                # Colocar logicas de conversão
-            elif output == '4':
-                print ('Opção 4 selecionada...')
-                # Colocar logicas de conversão
-            elif output == '5':
-                print ('Opção 5 selecionada...')
-                # Colocar logicas de conversão
-            elif output == '6':
-                print ('Opção 6 selecionada...')
-                # Colocar logicas de conversão
-            elif output == '7':
-                print ('Opção 7 selecionada...')
-                # Colocar logicas de conversão
+# def conversores(y):
+#     global ow, num, num_c, r, v, K, U, E, lE, lF, f, eF
 
-        conversores(y)
+#     print('Deseja fazer uma conversão? (S/N)')
+#     op = input()
+
+#     if op == 'S' or 's':
+#         ow = 1
+#     if ow == 1:
+#         print('Escolha o valor que deseja converter: ')
+#         x = input()
+#         if y == 1:
+#             if x == 1:
+#                 conversores2(r)
+#                 ow = 0
+#                 conversores(1)
+#             elif x == 2:
+#                 conversores2(v)
+#                 ow = 0
+#                 conversores(1)
+#             elif x == 3:
+#                 conversores2(K)
+#                 ow = 0
+#                 conversores(1)
+#             elif x == 4:
+#                 conversores2(U)
+#                 ow = 0
+#                 conversores(1)
+#             elif x == 5:
+#                 conversores2(E)
+#                 ow = 0
+#                 conversores(1)
+#             elif x == 6:
+#                 conversores2(lE)
+#                 ow = 0
+#                 conversores(1)
         
-    else:
-        input('Pressione Enter para continuar...')
+#         else:
+#             print('Opção inválida. Escolha uma opção válida.')
+
+#     else:
+#         input('Pressione Enter para continuar...')
         
-def escolhas1():
-    print('Digite o número da opção que deseja converter: ')
-    option = input()
-    if option == '1':
-        print('Opção 1 selecionada...')
-        conversores(r)
+# def escolhas1():
+#     print('Digite o número da opção que deseja converter: ')
+#     option = input()
+#     if option == '1':
+#         print('Opção 1 selecionada...')
+#         conversores(r)
 
-    elif option == '2':
-        print('Opção 2 selecionada...')
-        conversores(v)
+#     elif option == '2':
+#         print('Opção 2 selecionada...')
+#         conversores(v)
 
-    elif option == '3':
-        print('Opção 3 selecionada...')
-        conversores(K)
+#     elif option == '3':
+#         print('Opção 3 selecionada...')
+#         conversores(K)
 
-    elif option == '4':
-        print('Opção 4 selecionada...')
-        conversores(U)
+#     elif option == '4':
+#         print('Opção 4 selecionada...')
+#         conversores(U)
 
-    elif option == '5':
-        print('Opção 5 selecionada...')
-        conversores(E)
+#     elif option == '5':
+#         print('Opção 5 selecionada...')
+#         conversores(E)
 
-    elif option == '6':
-        print('Opção 6 selecionada...')
-        conversores(lE)
+#     elif option == '6':
+#         print('Opção 6 selecionada...')
+#         conversores(lE)
 
-    else:
-        print('Opção inválida. Escolha uma opção válida.')
+#     else:
+#         print('Opção inválida. Escolha uma opção válida.')
 
 # Opções do menu de cálculos
 
@@ -311,7 +269,7 @@ def opcao1():
     print(f'5 - Energia total (E): {E:.4g} eV')
     print(f'6 - Comprimento de onda do elétron (lE): {lE:.4g} m')
 
-    conversores(1)
+    # conversores(1)
     
     input('Pressione Enter para continuar...')
     limpar_variaveis()
